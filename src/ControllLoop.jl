846f667f09ff6cfd513aee7c95547896e01acf2f
Johann-Tobias Schäg <joto@l014.fem.tu-ilmenau.de>
2020-10-03 01:17:43 +0200

    Yikes, >8 minutes load time => Static Compilation strictly necessary.

--- src/ControllLoop.jl
+++ src/ControllLoop.jl
@@ -2,6 +2,12 @@ module ControllLoop
 
 using GameZero
 using OrdinaryDiffEq
-using Colors
+using Colors ## might not be necessary
+## https://github.com/aviks/GameZero.jl/blob/b8fe7b9e39f70c541c5715adc9e25559e9ab710f/example/BasicGame/basic2.jl#L7
+
+const Height, Width = 720, 720
+# check prelimary safe value from https://gs.statcounter.com/screen-resolution-stats/desktop/worldwide
+BACKGROUND=colorant"black"
+
 
 end  # module ControllLoop
